7ebf4599db499629b168ceee0ba0f59cac36a6d3
Stanislav Shadrov <Stasgrisha2@yandex.ru>
2022-11-28 18:15:23 +0300

    task2 is done

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ for (int i = -N; i <= N; i++)
 
 
 
-
+/*
 //Первая домашка
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 // a > 5; b < 7 -> max = 7; a > 2 b < 10 -> max = 10; a > -9 b < -3 -> max = -3
@@ -32,4 +32,58 @@ else
     Console.WriteLine($"{a} < {b}");
     Console.WriteLine($"max = {b}");
 }
+*/
 
+
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+Console.WriteLine("Ether number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ether number: ");
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());Console.WriteLine("Ether number: ");
+
+int max = -1;
+if (a == b && b == c){
+    max = a;
+    Console.WriteLine("Нечего сравнивать - все числа равны");
+}else if (a == b){
+    max = a;
+    if (c > max){
+        max = c;
+	Console.WriteLine("Максимальное число - третье");
+    } else {
+	Console.WriteLine("Максимальные числа - первое и второе");
+    }
+}else if (a == c){
+    max = a;
+    if (b > max){
+        max = b;
+	Console.WriteLine("Максимальное число - второе");
+    } else {
+	Console.WriteLine("Максимальные числа - первое и третье");
+    }
+}else if (b == c){
+    max = b;
+    if (a > max){
+        max = a;
+	Console.WriteLine("Максимальное число - первое");
+    } else {
+	Console.WriteLine("Максимальные числа - второе и третье");
+    }
+}else{
+    max = a;
+    if (b > max){
+        max = b;
+    }
+    if (c > max){
+        max = c;
+    }
+if (max == a) Console.WriteLine("Максимальное число - первое");
+if (max == b) Console.WriteLine("Максимальное число - второе");
+if (max == c) Console.WriteLine("Максимальное число - третье");
+}
+Console.WriteLine(max);
