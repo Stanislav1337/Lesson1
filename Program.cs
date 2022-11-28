6b5fa03a55385c7097d9b2e78f27110dab026f5d
Stanislav Shadrov <Stasgrisha2@yandex.ru>
2022-11-28 17:58:57 +0300

    task1 is already done

--- Program.cs
+++ Program.cs
@@ -4,23 +4,23 @@
 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 2 -> " -2, -1, 0, 1, 2" 
 */
 
-int N = int.Parse(Console.ReadLine());
+/*int N = int.Parse(Console.ReadLine());
 for (int i = -N; i <= N; i++)
 {
     Console.WriteLine(i);
-}
+}*/
 
 
 
 
-
+//Первая домашка
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 // a > 5; b < 7 -> max = 7; a > 2 b < 10 -> max = 10; a > -9 b < -3 -> max = -3
 
 Console.WriteLine("Ether number: ");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Ether number: ");
-int b = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());Console.WriteLine("Ether number: ");
 
 if(a > b)
 {
@@ -32,3 +32,4 @@ else
     Console.WriteLine($"{a} < {b}");
     Console.WriteLine($"max = {b}");
 }
+
